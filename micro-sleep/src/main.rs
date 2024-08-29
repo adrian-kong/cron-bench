4964d9830a4d0c7107a2282573074afb325f6560
adrian-kong <adrian.k1234567890@gmail.com>
2024-08-29 22:34:01 +1000

    Adding sample UDS listener for event wakeup

--- micro-sleep/src/main.rs
+++ micro-sleep/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::str::FromStr;
+use std::time::Duration;
+use chrono::Utc;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let schedule = cron::Schedule::from_str("* * * * *").unwrap();
+    let mut sched_iter = schedule.upcoming(Utc);
+    loop {
+        let next_time = sched_iter.next();
+        tokio::select! {
+            // _ = tokio::time::sleep_until() => {
+            //
+            // },
+            _ = tokio::time::sleep(Duration::from_secs(60)) => {
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
--- uds-cron/src/main.rs
+++ uds-cron/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::str::FromStr;
+use chrono::Utc;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let schedule = cron::Schedule::from_str("* * * * *").unwrap();
+    let sched_iter = schedule.upcoming(Utc);
+    #[cfg(unix)]
+    {
+        use tokio::net::UnixListener;
+        use std::time::Duration;
+
+        let listener = UnixListener::bind("/tmp/test.sock")?;
+        loop {
+            tokio::select! {
+                ret = listener.accept() => {
+                    match ret {
+                        Ok((stream, _)) => {
+                            // perform update
+                            println!("updating...")
+                        }
+                        Err(e) => {
+                            // println!("could not accept unix listener on subscription renewal sock due to {e}");
+                        }
+                    }
+                },
+                _ = tokio::time::sleep(Duration::from_secs(60)) => {
+                    break;
+                }
+            }
+        }
+    }
+    Ok(())
 }
